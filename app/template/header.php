a2427273b337a7cf016b849238f7f2db43585dec
kawada68 <nodoguro68@gmail.com>
2021-07-03 07:03:01 +0900

    ヘッダーをテンプレートファイルに切り出す

---
+++ app/template/header.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <div class="header__inner">
+        <h1 class="logo header__logo"><a href="index.php" class="logo__link">Error Memo</a></h1>
+        <nav class="nav header__nav">
+            <ul class="nav__menu">
+                <li class="nav__item"><a href="index.php" class="nav__link">トップページ</a></li>
+                <li class="nav__item"><a href="login.php" class="nav__link">ログイン</a></li>
+                <li class="nav__item"><a href="logout.php" class="nav__link">ログアウト</a></li>
+                <li class="nav__item"><a href="mypage.php" class="nav__link">マイページ</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
