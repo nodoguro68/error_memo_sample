7c82057cfd3e069cd4038a933f3c28b00c9d1983
kawada68 <nodoguro68@gmail.com>
2021-07-03 05:46:30 +0900

    font-awesome読み込み
    カテゴリーリスト、カテゴリーフォーム修正

--- app/index.php
+++ app/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>トップページ</title>
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 
@@ -25,46 +26,51 @@
     <main class="main">
         <div class="container">
 
-            <aside class="sidebar">
-
-                <ul class="sidebar-list">
-                    <li class="sidebar-list__item"><a href="" class="sidebar-list__link">ダミー</a></li>
-                    <li class="sidebar-list__item"><a href="" class="sidebar-list__link">ダミー</a></li>
-                    <li class="sidebar-list__item"><a href="" class="sidebar-list__link">ダミー</a></li>
-                    <li class="sidebar-list__item"><a href="" class="sidebar-list__link">ダミー</a></li>
-                    <li class="sidebar-list__item"><a href="" class="sidebar-list__link">ダミー</a></li>
-                </ul>
-
-                <form method="post" action="" class="form--category">
-                    <input type="text" name="category" class="form__input">
-                    <input type="submit" value="＋">
-                </form>
-
-            </aside>
-
             <div class="contents">
+                <aside class="sidebar__area">
 
-                <form method="post" action="" class="form--search">
-                    <input type="text" name="search" class="form__input">
-                    <input type="submit" value="検索">
-                </form>
+                    <ul class="category-list">
+                        <li class="category-list__item">
+                            <a href="" class="category-list__link">ダミー</a>
+                            <button class="btn-delete"><i class="far fa-trash-alt "></i></button>
+                        </li>
+                        <li class="category-list__item"><a href="" class="category-list__link">ダミー</a></li>
+                        <li class="category-list__item"><a href="" class="category-list__link">ダミー</a></li>
+                        <li class="category-list__item"><a href="" class="category-list__link">ダミー</a></li>
+                    </ul>
 
-                <ul class="list">
-                    <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
-                    <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
-                    <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
-                    <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
-                    <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
-                    <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
-                </ul>
+                    <form method="post" action="" class="form-category">
+                        <input type="text" name="category" class="form__input" placeholder="カテゴリーを追加">
+                        <input type="submit" value="＋" class="btn-add">
+                    </form>
 
-                <ul class="pagenation-list">
-                    <li class="pagenation-list__item"><a href="">&lt;</a></li>
-                    <li class="pagenation-list__item"><a href="">&gt;</a></li>
-                </ul>
+                </aside>
 
-                <div class="btn__link">
-                    <a href="register_error-memo.php" class="link">＋</a>
+                <div class="main__area">
+
+                    <form method="post" action="" class="form--search">
+                        <input type="text" name="search" class="form__input">
+                        <input type="submit" value="検索">
+                    </form>
+
+                    <ul class="list">
+                        <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
+                        <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
+                        <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
+                        <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
+                        <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
+                        <li class="list__item"><a href="" class="list__link">エラー<span class="date"></span></a></li>
+                    </ul>
+
+                    <ul class="pagenation-list">
+                        <li class="pagenation-list__item"><a href="">&lt;</a></li>
+                        <li class="pagenation-list__item"><a href="">&gt;</a></li>
+                    </ul>
+
+                    <div class="btn__link">
+                        <a href="register_error-memo.php" class="link">＋</a>
+                    </div>
+
                 </div>
 
             </div>
