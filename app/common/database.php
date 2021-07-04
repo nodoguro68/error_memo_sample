f6930a276195afeadc348d5f0e6800cdbd75ccae
kawada68 <nodoguro68@gmail.com>
2021-07-04 23:02:31 +0900

    DB接続関数作成

---
+++ app/common/database.php
@@ -0,0 +1,15 @@
+<?php 
+
+function dbConnect()
+{
+    $dsn = 'mysql:dbname=error_memo;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = 'root';
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    );
+    $dbh = new PDO($dsn, $user, $password, $options);
+    return $dbh;
+}
