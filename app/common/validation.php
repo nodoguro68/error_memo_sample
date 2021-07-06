58dabdb54f914d4a9f3c491ac66f2a2d9c0d2ff6
kawada68 <nodoguro68@gmail.com>
2021-07-06 20:26:02 +0900

    引数名、引数の渡し方を修正

--- app/common/validation.php
+++ app/common/validation.php
@@ -5,94 +5,94 @@ require_once 'message.php';
 /**
  * 必須入力チェック
  * 
- * @param array $msg
+ * @param array $err_msg
  * @param string $str
  * @param string $key
  */
-function validRequired(array $msg, string $str, string $key) {
+function validRequired(&$err_msg, string $str, string $key) {
     if(empty(trim($str))) {
-        $msg[$key] = ERR_MSG_REQUIER;
+        $err_msg[$key] = ERR_MSG_REQUIER;
     }
 }
 /**
  * 最大文字数チェック
  * 
- * @param array $msg
+ * @param array $err_msg
  * @param string $str
  * @param string $key
  * @param int $max
  */
-function validMaxLen(array $msg, string $str, string $key, int $max = 256) {
+function validMaxLen(&$err_msg, string $str, string $key, int $max = 256) {
     if(mb_strlen($str) > $max) {
-        $msg[$key] = ERR_MSG_MAX_LEN;
+        $err_msg[$key] = $max.ERR_MSG_MAX_LEN;
     }
 }
 /**
  * 最小文字数チェック
  * 
- * @param array $msg
+ * @param array $err_msg
  * @param string $str
  * @param string $key
  * @param int $min
  */
-function validMinLen(array $msg, string $str, string $key, int $min = 8) {
+function validMinLen(&$err_msg, string $str, string $key, int $min = 8) {
     if(mb_strlen($str) < $min) {
-        $msg[$key] = ERR_MSG_MIN_LEN;
+        $err_msg[$key] = $min.ERR_MSG_MIN_LEN;
     }
 }
 /**
  * 半角英数字チェック
  * 
- * @param array $msg
+ * @param array $err_msg
  * @param string $str
  * @param string $key
  */
-function validHalf(array $msg, string $str, string $key) {
+function validHalf(&$err_msg, string $str, string $key) {
     if(!preg_match("/^[a-zA-Z0-9]+$/", $str)) {
-        $msg[$key] = ERR_MSG_HALF;
+        $err_msg[$key] = ERR_MSG_HALF;
     }
 }
 /**
  * メールアドレス形式チェック
  * 
- * @param array $msg
+ * @param array $err_msg
  * @param string $mail
- * @param string $key
  */
-function validMail(array $msg, string $mail, string $key) {
+function validMail(&$err_msg, string $mail) {
     if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $mail)) {
-        $msg[$key] = ERR_MSG_MAIL;
+        $err_msg['mail_address'] = ERR_MSG_MAIL;
     }
 }
 /**
  * メールアドレス重複チェック
  * 
- * @param array $msg
+ * @param array $err_msg
  * @param string $mail
- * @param string $key
  */
-function validMailDup(array $msg, string $mail_address, string $key) {
+function validMailDup(&$err_msg, string $mail_address) {
+
+    $dbh = dbConnect();
 
     $result = fetch(
+        $dbh,
         'SELECT count(*) FROM users WHERE mail_address = :mail_address AND delete_flg = 0',
         array(':mail_address' => $mail_address)
     );
     
     if(!empty(array_shift($result))) {
-        $msg[$key] = ERR_MSG_MAIL_DUP;
+        $err_msg['mail_address'] = ERR_MSG_MAIL_DUP;
     }
 }
 /**
  * パスワード再入力チェック
  * 
- * @param array $msg
+ * @param array $err_msg
  * @param string $pass
  * @param string $pass_re
- * @param string $key
  */
-function validPassRe(array $msg, string $pass, string $pass_re, string $key) {
+function validPassRe(&$err_msg, string $pass, string $pass_re) {
     if($pass !== $pass_re) {
-        $msg[$key] = ERR_MSG_PASS_RE;
+        $err_msg['password'] = ERR_MSG_PASS_RE;
     }
 }
 
