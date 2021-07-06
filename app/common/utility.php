123f5c7b80b34da90507522daeffd888f62fb259
kawada68 <nodoguro68@gmail.com>
2021-07-06 20:28:53 +0900

    validation.phpを読み込まない
    DB関連の関数の引数を修正
    
    関数の型を宣言
    
    エラーを表示する設定
    
    フォームの入力保持を行う関数作成

--- app/common/utility.php
+++ app/common/utility.php
@@ -1,7 +1,6 @@
 <?php 
 
 require_once 'database.php';
-require_once 'validation.php';
 
 $err_msg = array();
 
@@ -12,8 +11,7 @@ $err_msg = array();
  * @param array $params
  * @return object
  */
-function execute($sql,$params = array()) {
-    $dbh = dbConnect();
+function execute(object $dbh, string $sql,array $params) {
 
     $stmt = $dbh->prepare($sql);
     $stmt->execute($params);
@@ -28,8 +26,8 @@ function execute($sql,$params = array()) {
  * @param array $params
  * @return mixed
  */
-function fetch($sql, $params = array()) {
-    return $result = execute($sql, $params)->fetch(PDO::FETCH_ASSOC);
+function fetch(object $dbh, string $sql, array $params) {
+    return $result = execute($dbh, $sql, $params)->fetch(PDO::FETCH_ASSOC);
 }
 /**
  * 全レコードを取得
@@ -38,13 +36,14 @@ function fetch($sql, $params = array()) {
  * @param array $params
  * @return mixed
  */
-function fetchAll($sql, $params = array()) {
-    return execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+function fetchAll(object $dbh, string $sql, array $params) {
+    return execute($dbh, $sql, $params)->fetchAll(PDO::FETCH_ASSOC);
 }
 
 // エラーログ
 ini_set('log_errors', 'on');
-ini_set('error_log', '../php.log');
+ini_set('error_log', '../../php.log');
+ini_set('display_errors', "On");
 
 // セッション
 session_start();
@@ -62,12 +61,29 @@ function escape($str) {
 /**
  * エラーメッセージ表示
  * 
- * @param string $err_msg
+ * @param array $err_msg
  * @param string $key
  * @return array
  */
-function getErrMsg(string $err_msg, string $key) {
+function getErrMsg(&$err_msg, string $key) {
     if(!empty($err_msg[$key])) {
         return $err_msg[$key];
+    }
+}
+
+/**
+ * フォームの入力保持
+ * 
+ * @param string $key
+ * @param bool $flg
+ */
+function getFormData(string $key, $flg = false) {
+    if($flg) {
+        $method = $_GET;
+    } else {
+        $method = $_POST;
+    }
+    if(!empty($method[$key])) {
+        return escape($method[$key]);
     }
 }
