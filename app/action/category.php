db09004552f81c52f1e03b2b1869f85095593810
kawada68 <nodoguro68@gmail.com>
2021-07-08 18:24:00 +0900

    カテゴリー削除関数作成

--- app/action/category.php
+++ app/action/category.php
@@ -57,3 +57,32 @@ function getCategories(&$err_msg, int $user_id) {
     }
 }
 
+/**
+ * カテゴリー削除
+ * 
+ * @param array $err_msg
+ * @param int $category_id
+ * @param int $user_id
+ */
+function deleteCategory(&$err_msg, int $category_id, int $user_id) {
+
+    try{
+
+        $dbh = dbConnect();
+
+        $sql = 'DELETE FROM categories WHERE category_id = :category_id AND user_id = :user_id';
+        $data = array(
+            ':category_id' => $category_id,
+            ':user_id' => $user_id,
+        );
+
+        if (execute($dbh, $sql, $data)) {
+            header('Location: index.php');
+        }
+
+    } catch(Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
+
