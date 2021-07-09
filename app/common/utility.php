7e0c440f6e448922a351cd8e4fc6633c8904a728
kawada68 <nodoguro68@gmail.com>
2021-07-09 09:32:20 +0900

    セッションの設定を追加

--- app/common/utility.php
+++ app/common/utility.php
@@ -46,6 +46,9 @@ ini_set('error_log', '../../php.log');
 ini_set('display_errors', "On");
 
 // セッション
+session_save_path("/var/tmp/");
+ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 30);
+ini_set('session.cookie_lifetime ', 60 * 60 * 24 * 30);
 session_start();
 session_regenerate_id();
 
